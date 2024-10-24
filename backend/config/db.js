097033086ade9ab8825dc48a41e0f9bfceea2396
Lâm Bá Luân <lambaluan2609@gmail.com>
2024-10-25 02:54:42 +0700

    create place order using stripe

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://lambaluan2609:11111@cluster0.iptid.mongodb.net/Food-Del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
 }

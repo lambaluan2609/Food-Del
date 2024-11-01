a208c8368e3b564ad2ddaa09b1e55a12adbae252
Lâm Bá Luân <lambaluan2609@gmail.com>
2024-11-01 10:56:59 +0700

    reverse order item

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -13,7 +13,7 @@ const Orders = ({url}) => {
   const fetchAllOrders = async () => {
     const response = await axios.get(url+"/api/order/list")
     if (response.data.success) {
-      setOrders(response.data.data);
+      setOrders(response.data.data.reverse());
       console.log(response.data.data);
       
     }

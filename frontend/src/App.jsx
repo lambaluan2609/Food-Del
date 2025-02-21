bcb6c71b03e63910ac154b5990e4039f9e0bc532
DangNH <haidang12112002@gmail.com>
2025-02-21 18:26:29 +0700

    frontend details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,25 +16,26 @@ const App = () => {
 
   return (
     <>
-      {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-      <Navbar setShowLogin={setShowLogin}/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-        <Route path='/verify' element={<Verify/>}/>
-        <Route path='/qrcode' element={<QRCodePage/>}/>
-        <Route path='/myorders' element={<MyOrders/>}/>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/detail/:id' element={<RecipeDetails />} /> */}
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/verify' element={<Verify />} />
+          <Route path='/qrcode' element={<QRCodePage />} />
+          <Route path='/myorders' element={<MyOrders />} />
 
 
-         <Route path="*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/" />} />
 
-      </Routes>
+        </Routes>
       </div>
       <Footer />
     </>
-    
+
   )
 }
 

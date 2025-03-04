14517395080158a4808d83c90d2f9af72f8f3b03
DangNH <haidang12112002@gmail.com>
2025-03-04 17:54:57 +0700

    hiddem price in receipt

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -4,7 +4,7 @@ import './FoodItem.css'
 import { StoreContext } from '../../context/StoreContext'
 import { useNavigate } from 'react-router-dom'
 
-const FoodItem = ({ id, name, price, description, image }) => {
+const FoodItem = ({ id, name, description, image }) => {
 
     const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
     const navigate = useNavigate()
@@ -14,20 +14,20 @@ const FoodItem = ({ id, name, price, description, image }) => {
             <div className="food-item-image-container">
                 <img className='food-item-image' src={image} alt="" />
                 {!cartItems[id]
-                    ? <img className='add' onClick={(e) => { 
-                            e.stopPropagation(); // Ngăn không trigger navigate
-                            addToCart(id) 
-                        }} src={assets.add_icon_white} alt="" />
+                    ? <img className='add' onClick={(e) => {
+                        e.stopPropagation(); // Ngăn không trigger navigate
+                        addToCart(id)
+                    }} src={assets.add_icon_white} alt="" />
                     : <div className='food-item-counter'>
-                        <img onClick={(e) => { 
-                                e.stopPropagation(); 
-                                removeFromCart(id) 
-                            }} src={assets.remove_icon_red} alt="" />
+                        <img onClick={(e) => {
+                            e.stopPropagation();
+                            removeFromCart(id)
+                        }} src={assets.remove_icon_red} alt="" />
                         <p>{cartItems[id]}</p>
-                        <img onClick={(e) => { 
-                                e.stopPropagation(); 
-                                addToCart(id) 
-                            }} src={assets.add_icon_green} alt="" />
+                        <img onClick={(e) => {
+                            e.stopPropagation();
+                            addToCart(id)
+                        }} src={assets.add_icon_green} alt="" />
                     </div>
                 }
             </div>
@@ -37,7 +37,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
                     <img src={assets.rating_starts} alt="" />
                 </div>
                 <p className="food-item-desc">{description}</p>
-                <p className="food-item-price">${price}</p>
+
 
             </div>
         </div>
